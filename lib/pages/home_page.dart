d90fb22769ea8671951424009f1071391ad8dcd1
Toretay <abdymvzhit@gmail.com>
2025-05-02 15:06:49 +0500

    changed the images appearance in the code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,17 +10,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<String> images = [
+    "images/1_image.jpg",
+    "images/2_image.jpg",
+    "images/3_image.jpg",
+    "images/4_image.jpg",
+    "images/5_image.jpg",
+    "images/6_image.jpg",
+  ];
 
   @override
   Widget build(BuildContext context) {
+    final String selectedImage = images[Random().nextInt(images.length)];
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("images/1_image.jpg"),
-            fit: BoxFit.cover,
-          ),
-        ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [Image.asset(selectedImage, fit: BoxFit.cover)],
       ),
     );
   }
