10d74482ba4362458252c301347a88a172340e6a
Toretay <abdymvzhit@gmail.com>
2025-05-06 15:29:52 +0500

    changed provider.of to consumer<QuoteProvider>

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,90 +7,99 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final quoteProvider = Provider.of<QuoteProvider>(context);
-    final quote = quoteProvider.quote;
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Container(
-        width: double.maxFinite,
-        height: double.maxFinite,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(quoteProvider.selectedImage),
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(Colors.black45, BlendMode.darken),
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
-          child:
-              quoteProvider.isLoading || quote == null
-                  ? Center(
-                    child: CircularProgressIndicator(color: Colors.white),
-                  )
-                  : Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.format_quote,
-                        size: 100.0,
-                        color: Colors.white,
-                      ),
-                      Text(
-                        quote.quote,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: Colors.white, fontSize: 30.0),
-                      ),
-                      SizedBox(height: 12.0),
-                      Row(
+    return Consumer<QuoteProvider>(
+      builder: (context, quoteProvider, child) {
+        final quote = quoteProvider.quote;
+        return Scaffold(
+          backgroundColor: Colors.black,
+          body: Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(quoteProvider.selectedImage),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(Colors.black45, BlendMode.darken),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 12.0,
+              ),
+              child:
+                  quoteProvider.isLoading || quote == null
+                      ? Center(
+                        child: CircularProgressIndicator(color: Colors.white),
+                      )
+                      : Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
                         children: [
-                          Icon(Icons.person, color: Colors.white),
-                          SizedBox(width: 6.0),
+                          Icon(
+                            Icons.format_quote,
+                            size: 100.0,
+                            color: Colors.white,
+                          ),
                           Text(
-                            quote.author,
+                            quote.quote,
                             textAlign: TextAlign.center,
                             style: TextStyle(
                               color: Colors.white,
-                              fontSize: 18.0,
+                              fontSize: 30.0,
                             ),
+                          ),
+                          SizedBox(height: 12.0),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(Icons.person, color: Colors.white),
+                              SizedBox(width: 6.0),
+                              Text(
+                                quote.author,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 12.0),
+                          Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  quoteProvider.nextQuote();
+                                },
+                                icon: Icon(
+                                  Icons.restart_alt,
+                                  color: Colors.white,
+                                  size: 40.0,
+                                ),
+                              ),
+                              SizedBox(width: 12.0),
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.favorite_border,
+                                  color: Colors.white,
+                                  size: 40.0,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                      SizedBox(height: 12.0),
-                      Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          IconButton(
-                            onPressed: () {
-                              quoteProvider.nextQuote();
-                            },
-                            icon: Icon(
-                              Icons.restart_alt,
-                              color: Colors.white,
-                              size: 40.0,
-                            ),
-                          ),
-                          SizedBox(width: 12.0),
-                          IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.favorite_border,
-                              color: Colors.white,
-                              size: 40.0,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-        ),
-      ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
