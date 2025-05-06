f6e8898ec51c745f150d6f1854520c37890ad83a
Toretay <abdymvzhit@gmail.com>
2025-05-06 15:07:35 +0500

    created quote_provider.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,48 +14,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final List<String> images = [
-    "images/1_image.jpg",
-    "images/2_image.jpg",
-    "images/3_image.jpg",
-    "images/4_image.jpg",
-    "images/5_image.jpg",
-    "images/6_image.jpg",
-  ];
-  late String _selectedImage;
-
-  final Random random = Random();
-
-  List<QuoteModel> _quotes = [];
-
-  int _index = 0;
+  
 
   @override
   void initState() {
     super.initState();
-    _selectedImage = images[random.nextInt(images.length)];
-    getRandomQuotes();
   }
-
-  Future<void> getRandomQuotes() async {
-    _quotes = [];
-    _index = 0;
-    final Uri url = Uri.https("zenquotes.io", "/api/quotes");
-    try {
-      final response = await http.get(url);
-      if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
-        for (var i in data) {
-          final QuoteModel quote = QuoteModel(quote: i["q"], author: i["a"]);
-          _quotes.add(quote);
-        }
-        setState(() {});
-      }
-    } catch (e) {
-      print("Error fetching quotes: $e");
-    }
-  }
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
---
+++ lib/quote_provider.dart
@@ -0,0 +1,57 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:daily_quote_app/models/quote_model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class QuoteProvider extends ChangeNotifier {
+  QuoteProvider() {
+    selectRandomImage();
+    getRandomQuotes();
+  }
+
+  final List<String> images = [
+    "images/1_image.jpg",
+    "images/2_image.jpg",
+    "images/3_image.jpg",
+    "images/4_image.jpg",
+    "images/5_image.jpg",
+    "images/6_image.jpg",
+  ];
+  late String _selectedImage;
+
+  final Random random = Random();
+
+  List<QuoteModel> _quotes = [];
+
+  int _index = 0;
+
+  QuoteModel get quote => _quotes[_index];
+  String get selectedImage => _selectedImage;
+  int get index => _index;
+
+  void selectRandomImage() {
+    _selectedImage = images[random.nextInt(images.length)];
+    notifyListeners();
+  }
+
+  Future<void> getRandomQuotes() async {
+    _quotes = [];
+    _index = 0;
+    final Uri url = Uri.https("zenquotes.io", "/api/quotes");
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        for (var i in data) {
+          final QuoteModel quote = QuoteModel(quote: i["q"], author: i["a"]);
+          _quotes.add(quote);
+        }
+        notifyListeners();
+      }
+    } catch (e) {
+      print("Error fetching quotes: $e");
+    }
+  }
+}
