2e140844e6734aaf857c248fb26844b303f153e2
Toretay <abdymvzhit@gmail.com>
2025-05-12 11:40:48 +0500

    created add/remove quote, isFavorite functions

--- lib/provider/favorite_quote_provider.dart
+++ lib/provider/favorite_quote_provider.dart
@@ -8,4 +8,18 @@ class FavoriteQuoteProvider extends ChangeNotifier {
   Future<void> initBox() async {
     _favQuotesBox = await Hive.openBox<FavoriteQuoteModel>("favQuotesBox");
   }
+
+  Future<void> addQuote(FavoriteQuoteModel quote) async {
+    await _favQuotesBox.put("key_${quote.author}_${quote.quote}", quote);
+    notifyListeners();
+  }
+
+  Future<void> removeQuote(FavoriteQuoteModel quote) async {
+    await _favQuotesBox.delete("key_${quote.author}_${quote.quote}");
+    notifyListeners();
+  }
+
+  bool isFavorite(FavoriteQuoteModel quote) {
+    return _favQuotesBox.containsKey("key_${quote.author}_${quote.quote}");
+  }
 }
