3e568d67d61cd14664bc5950e8aa80291b9fabdd
Toretay <abdymvzhit@gmail.com>
2025-05-12 12:39:02 +0500

    added listview to output favs.

--- lib/pages/favorites_page.dart
+++ lib/pages/favorites_page.dart
@@ -1,3 +1,5 @@
+import 'package:daily_quote_app/boxes.dart';
+import 'package:daily_quote_app/models/favorite_quote_model.dart';
 import 'package:flutter/material.dart';
 
 class FavoritesPage extends StatelessWidget {
@@ -7,6 +9,13 @@ class FavoritesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Favorite quotes"), centerTitle: true),
+      body: ListView.builder(
+        itemCount: favQuotesBox.length,
+        itemBuilder: (context, index) {
+          final FavoriteQuoteModel favQuote = favQuotesBox.getAt(index);
+          return ListTile(title: Text(favQuote.quote));
+        },
+      ),
     );
   }
 }
--- lib/provider/favorite_quote_provider.dart
+++ lib/provider/favorite_quote_provider.dart
@@ -1,7 +1,6 @@
 import 'package:daily_quote_app/boxes.dart';
 import 'package:daily_quote_app/models/favorite_quote_model.dart';
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
 class FavoriteQuoteProvider extends ChangeNotifier {
   Future<void> addRemoveQuote(FavoriteQuoteModel quote) async {
