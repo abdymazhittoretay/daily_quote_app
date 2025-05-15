f6f4cd35e779d842a6f3b1c58e68c09a0442f6c9
Toretay <abdymvzhit@gmail.com>
2025-05-15 15:11:16 +0500

    added background image

--- lib/pages/favorites_page.dart
+++ lib/pages/favorites_page.dart
@@ -1,5 +1,6 @@
 import 'package:daily_quote_app/models/favorite_quote_model.dart';
 import 'package:daily_quote_app/provider/favorite_quote_provider.dart';
+import 'package:daily_quote_app/provider/quote_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:provider/provider.dart';
@@ -9,54 +10,70 @@ class FavoritesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Favorite quotes"), centerTitle: true),
-      body: Consumer<FavoriteQuoteProvider>(
-        builder: (context, favQuoteProvider, child) {
-          final Box favQuotesBox = favQuoteProvider.favQuotesBox;
-          if (favQuotesBox.isEmpty) {
-            return Center(
-              child: Text(
-                "There are no favorites yet.",
-                style: TextStyle(fontSize: 16.0),
-              ),
-            );
-          }
-          final List<dynamic> favQuotes = favQuotesBox.values.toList();
-
-          favQuotes.sort((a, b) => b.dateTime.compareTo(a.dateTime));
-
-          return ListView.builder(
-            itemCount: favQuotes.length,
-            itemBuilder: (context, index) {
-              final FavoriteQuoteModel favQuote = favQuotes[index];
-              return ListTile(
-                contentPadding: EdgeInsets.only(
-                  left: 16.0,
-                  right: 8.0,
-                  top: 4.0,
-                  bottom: 4.0,
-                ),
-                title: Text(favQuote.quote, style: TextStyle(fontSize: 18.0)),
-                subtitle: Text(
-                  favQuote.author,
-                  style: TextStyle(color: Colors.grey, fontSize: 16.0),
-                ),
-                trailing: IconButton(
-                  onPressed: () {
-                    favQuoteProvider.addRemoveQuote(favQuote);
-                  },
-                  iconSize: 30.0,
-                  color: Colors.red,
-                  icon:
-                      favQuoteProvider.isFavorite(favQuote)
-                          ? Icon(Icons.favorite)
-                          : Icon(Icons.favorite_border),
+    return Container(
+      width: double.maxFinite,
+      height: double.maxFinite,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(context.watch<QuoteProvider>().selectedImage),
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(Colors.black45, BlendMode.darken),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Text("Favorite quotes"),
+          centerTitle: true,
+        ),
+        body: Consumer<FavoriteQuoteProvider>(
+          builder: (context, favQuoteProvider, child) {
+            final Box favQuotesBox = favQuoteProvider.favQuotesBox;
+            if (favQuotesBox.isEmpty) {
+              return Center(
+                child: Text(
+                  "There are no favorites yet.",
+                  style: TextStyle(fontSize: 16.0),
                 ),
               );
-            },
-          );
-        },
+            }
+            final List<dynamic> favQuotes = favQuotesBox.values.toList();
+
+            favQuotes.sort((a, b) => b.dateTime.compareTo(a.dateTime));
+
+            return ListView.builder(
+              itemCount: favQuotes.length,
+              itemBuilder: (context, index) {
+                final FavoriteQuoteModel favQuote = favQuotes[index];
+                return ListTile(
+                  contentPadding: EdgeInsets.only(
+                    left: 16.0,
+                    right: 8.0,
+                    top: 4.0,
+                    bottom: 4.0,
+                  ),
+                  title: Text(favQuote.quote, style: TextStyle(fontSize: 18.0)),
+                  subtitle: Text(
+                    favQuote.author,
+                    style: TextStyle(color: Colors.grey, fontSize: 16.0),
+                  ),
+                  trailing: IconButton(
+                    onPressed: () {
+                      favQuoteProvider.addRemoveQuote(favQuote);
+                    },
+                    iconSize: 30.0,
+                    color: Colors.red,
+                    icon:
+                        favQuoteProvider.isFavorite(favQuote)
+                            ? Icon(Icons.favorite)
+                            : Icon(Icons.favorite_border),
+                  ),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
